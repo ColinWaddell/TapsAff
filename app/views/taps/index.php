e68f759a0cc04145a5e6a163147f267ab2979aa0
Colin Waddell <mrcolin@gmail.com>
2013-05-30 17:09:12 +0100

    Added taps definition

--- app/views/taps/index.php
+++ app/views/taps/index.php
@@ -15,6 +15,16 @@
     <?php if (($status->temp_f >= $GLOBALS['taps_temp'] - 5) && ($status->temp_f <= $GLOBALS['taps_temp'])): ?>
       <p class="close">...but it's close!</p>
     <?php endif; ?>
-</div> <!-- taps-text-area -->
+
+    <div class="offset-by-one ten columns alpha omega">
+      <p class="taps-definition">
+        <span class="b">Taps-Aff (Scots Vernacular) </span>Literally "tops off."
+        The removing of one's shirt in the event of warm weather, a phenomenon rarely seen in Glasgow.
+        Now an expression describing good times being had.<a href="http://www.urbandictionary.com/define.php?term=taps+aff" target="_blank" class="superscript">1</a>  <br /><br />
+        <span class="b">Antonym:</span> Taps-Oan, "tops on".
+      </p>
+    </div>
+
+  </div> <!-- taps-text-area -->
 
 
