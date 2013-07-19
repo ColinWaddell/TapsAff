e4d690f4975fc83a59785f325baed1b2b8718543
Colin Waddell <mrcolin@gmail.com>
2013-07-20 00:05:55 +0100

    tidied up search form

--- app/views/search/index.php
+++ app/views/search/index.php
@@ -1,12 +1,15 @@
 
-
+<h4>Where are ye...?</h4>
 <form role="search" id="locationsearch" method="get" action="<?php echo myUrl('',true); ?>" autocomplete="off">
   <input name="location" 
          id="location" 
-        placeholder="<?php 
-          if (isset($status->place_error) && $status->place_error != '')
-            echo $status->place_error;
-          else
-            echo 'Where are ye...?';
-        ?>">
+        <?php 
+          if (isset($status->place_error) && $status->place_error != ''){
+            echo 'placeholder="'.$status->place_error.'" ';
+            echo 'class="notfound"';
+          }else{
+            echo 'placeholder="Check anywhere in the UK!"';
+          };
+        ?>
+  >
 </form>
